e2bf717a61708d9631f88d9eff18e66fc6d7bfaa
farhanSillaGit <133650383+farhanSillaGit@users.noreply.github.com>
2024-05-17 23:44:46 +0400

    Update utils.py

--- testpro/home/utils.py
+++ testpro/home/utils.py
@@ -1,9 +1,11 @@
+from __future__ import absolute_import, unicode_literals
+from celery import shared_task
 import os
 import subprocess
 from django.conf import settings
 
 BASE_DIR = settings.BASE_DIR
-
+@shared_task
 def transcode_to_mpd(video_file_path):
     # Define the output directory for MPD files
     output_directory = os.path.join(BASE_DIR, 'videos')
@@ -16,8 +18,8 @@ def transcode_to_mpd(video_file_path):
 
     # Use FFmpeg to transcode the video to MPD format
 
-    #ffmpeg_executable = r'C:\ffmpeg\ffmpeg-2024-05-13-git-37db0454e4-essentials_build\bin\ffmpeg.exe'
-    command = ['ffmpeg', '-i', video_file_path, '-c:v', 'libx264', '-preset', 'fast', '-c:a', 'aac', '-strict', 'experimental', '-f', 'dash', output_mpd_file]
+    ffmpeg_executable = 'ffmpeg'
+    command = [ffmpeg_executable,'-i', video_file_path, '-c:v', 'libx264', '-preset', 'fast', '-c:a', 'aac', '-strict', 'experimental', '-f', 'dash', output_mpd_file]
 
 
     try:
